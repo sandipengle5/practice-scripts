b46c732c603886f4ed2059ca80c554ff9f2ecd24
Sandip Engle <ubuntu@ip-172-31-44-91.eu-north-1.compute.internal>
2026-09-19 01:01:13 +0000

    calculator updated with addition and substraction

--- calculator_new.sh
+++ calculator_new.sh
@@ -2,4 +2,19 @@
 
 #addition fuction
 
-x=1+2
+echo "enter 1st number"
+rean num1
+
+echo "enter 2nd number"
+read num2
+
+echo "choose operation +, -, *, /"
+read op
+
+case $op in
+	+) 
+		echo "result =$(($num1 + $num2))"
+		;;
+	-)
+		echo "result = $(($num1 - $num2))"
+		;;

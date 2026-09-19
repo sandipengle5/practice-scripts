dd62fdeb0a75f542269cd4a232898ac83e8ae8b6
Sandip Engle <ubuntu@ip-172-31-44-91.eu-north-1.compute.internal>
2026-09-19 00:17:49 +0000

    added calculator_new.sh script to github account

--- aws-resource-tracker.sh
+++ aws-resource-tracker.sh
@@ -18,17 +18,20 @@ set -x #debug mode
 #AWS Lambda
 #AWS IAM Users
 
+#date 
+date >> /home/ubuntu/git-practice/ResourceTracker
+
 #list s3 buckets
 echo "print list of s3 buckets"
-aws s3 ls > ResourceTracker 
+aws s3 ls  >> ResourceTracker 
 
 #list EC2 instance
 echo "print list ES2 instance"
-aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId' >> ResourceTracker
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'  >> ResourceTracker
 
 #list AWS Lambda
 echo "print list lambda fuctions"
-aws lambda list-functions >> ResourceTracker
+aws lambda list-functions  >> ResourceTracker
 
 #list IAM users
 echo "print list IAM users"
